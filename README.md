e57c82ae98dbb910d462ea82bca4a922cf1e07c4
Carlos Chacin <cchacin@gmail.com>
2020-09-15 14:06:34 -0700

    Add instructions and improve the install script

--- README.md
+++ README.md
@@ -1,6 +1,53 @@
 # dotfiles
-How to install:
+
+## What's in there
+
+### OhMyZsh
+
+Configured using [antigen]() and [powerlevel10k]() theme
+
+OhMyZSH Plugins:
+
+- ssh-agent
+- gpg-agent
+- autojump
+- brew
+- brew-cask
+- colored-man-pages
+- common-aliases
+- docker
+- docker-compose
+- git
+- git-extras
+- git-hubflow
+- git-remote-branch
+- gitignore
+- heroku
+- history
+- httpie
+- mvn
+- sudo
+- rbenv
+- jenv
+- zsh-users/zsh-syntax-highlighting
+
+### Homebrew
+
+Homebrew formulas and casks installed and backed up in a bundle: `Brewfile`
+
+- `brew bundle install` to install all the formulas and casks
+- `brew bundle dump -f` to regenerate the `Brewfile`
+
+### Git
+- Global `.gitconfig`
+- Global `.gitignore`
+
+### Java
+- JDKs installed with Hombrew
+- [jEnv]() installed with Hombrew
+- Global `.mavenrc` using [jEnv]()
+
+## How to install:
 ```sh
 $ bash -c "$(curl -fsSL https://raw.githubusercontent.com/cchacin/dotfiles/master/scripts/install.sh)"
 ```
-Paste that at a Terminal prompt.
--- scripts/install.sh
+++ scripts/install.sh
@@ -9,43 +9,38 @@ echo " | (_| | (_) | ||__|  _| | |  __/\__ \\"
 echo "  \__,_|\___/ \__| |_| |_|_|\___||___/"
 echo ""
 
-echo "==> Starting..."
+echo "==> 🚧 Starting..."
 cd "$HOME"
 DOTFILES=${HOME}/dotfiles
 
 if [ -d "${DOTFILES}" ]; then
-  echo "==> Fetching  and Pulling latest dotfiles..."
-  cd "${DOTFILES}" && git fetch > /dev/null && git pull > /dev/null
+  echo "  ==> ✅ Dotfiles already present"
+  echo "  ==> 🚧 Fetching and Pulling latest dotfiles..."
+  cd "${DOTFILES}" && git pull origin master > /dev/null
 else
-  echo "==> Cloning dotfiles..."
+  echo "  ==> 🚧 Cloning Dotfiles..."
   git clone https://github.com/cchacin/dotfiles.git
+  cd "${DOTFILES}"
 fi
 
-if [[ $OSTYPE == darwin* ]]; then
-  # if homebrew exists install all the formulas
+if [ $OSTYPE == darwin* ]; then
+  echo "  ==> 🚧 Configuring MacOS..."
   if which brew > /dev/null; then
-    echo "==> brew already installed..."
+    echo "    ==> ✅ homebrew is already installed"
   else
-    echo "==> Installing homebrew"
+    echo "    ==> 🚧 Installing homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   fi
 
-  echo "==> Installing homebrew formulas"
+  echo "    ==> 🚧 Installing homebrew formulas"
   brew bundle install
 fi
 
-echo "==> Linking DotFiles to ${HOME}"
-ln -s "${DOTFILES}"/.aliases.sh "${HOME}"/.
-ln -s "${DOTFILES}"/.gitconfig "${HOME}"/.
-ln -s "${DOTFILES}"/.ghci "${HOME}"/.
-ln -s "${DOTFILES}"/.gitignore "${HOME}"/.
-ln -s "${DOTFILES}"/.mavenrc "${HOME}"/.
-ln -s "${DOTFILES}"/.p10k.zsh "${HOME}"/.
-ln -s "${DOTFILES}"/.zlogin "${HOME}"/.
-ln -s "${DOTFILES}"/.zprofile "${HOME}"/.
-ln -s "${DOTFILES}"/.zshenv "${HOME}"/.
-ln -s "${DOTFILES}"/.zshrc "${HOME}"/.
-ln -s "${DOTFILES}"/.antigenrc "${HOME}"/.
-ln -s "${DOTFILES}"/antigen.zsh "${HOME}"/.
+echo "  ==> 🚧 Linking Dotfiles to ${HOME}..."
+for filename in $(ls -A ${DOTFILES}/files);
+do
+  ln -sf "${DOTFILES}/files/${filename}" "${HOME}"/.
+  echo "    ==> ✅ Linked ${filename}";
+done;
 
-echo "==> Done."
+echo "==> ✅ Done."
