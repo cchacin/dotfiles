f7dd127b95f90d2e5c9af5065eb63955b1923780
Carlos Chacin <cchacin@gmail.com>
2024-05-03 07:03:03 -0700

    Brewfile + FZF + EXA config

--- aliases/.aliases.sh
+++ aliases/.aliases.sh
@@ -1,11 +1,6 @@
 # Git
 alias gitlog="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
-gdiff() {
-  preview="git diff $@ --color=always -- {-1}"
-  git diff $@ --name-only | fzf -m --ansi --preview $preview
-}
-
 # Finder
 alias showFiles="defaults write com.apple.finder AppleShowAllFiles YES;killall Finder /System/Library/CoreServices/Finder.app"
 alias hideFiles="defaults write com.apple.finder AppleShowAllFiles NO;killall Finder /System/Library/CoreServices/Finder.app"
@@ -18,13 +13,10 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 
-jwtd() {
-  h=`jq -R 'split(".") | .[0] | @base64d | fromjson' <<< "$1"` > /dev/null
-  p=`jq -R 'split(".") | .[1] | @base64d | fromjson' <<< "$1"` > /dev/null
-  echo "Header:\n"
-  echo $h | jq
-  echo "\nPayload:\n"
-  echo $p | jq
-}
-
 alias vim=nvim
+alias vi=nvim
+
+alias cat="bat --color=always --style=numbers"
+
+alias ls="eza --color=always --long --git --icons=always --no-time --no-user --no-permissions"
+alias lst="eza --tree --level=2"
